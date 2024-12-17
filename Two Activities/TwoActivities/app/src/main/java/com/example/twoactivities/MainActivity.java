d6c55af0f671bd8427ce15b1692b12465f47a324
dexmkns <dextervalencia77@gmail.com>
2024-12-17 21:32:07 +0800

    Update .gitignore and project configurations

---
+++ Two Activities/TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.twoactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+
+    public static final String EXTRA_MESSAGE=
+            "com.example.android.twoactivities.extra.MESSAGE";
+    public static final int TEXT_REQUEST=1;
+    private EditText mMessageEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                TextView replyHeaderTextView = findViewById(R.id.text_header_reply);
+                TextView replyTextView = findViewById(R.id.text_message_reply);
+                replyHeaderTextView.setVisibility(View.VISIBLE);
+                replyTextView.setVisibility(View.VISIBLE);
+                replyTextView.setText(reply);
+            }
+        }
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+
+}
